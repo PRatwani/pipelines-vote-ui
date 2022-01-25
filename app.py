95031e49ef9826a162b5757c4fa342263d8dc388
Pawan Ratwani <pawan.ratwani@gmail.com>
2022-01-25 09:02:46 +0000

    .

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request, make_response, g, send_from_directory
+
+"""[summary]
+
+    Returns:
+        [type]: [description]
+"""
 import os
 import socket
 import random
 import json
 import requests
+from flask import Flask, render_template, request, make_response, send_from_directory
 
-option_a = os.getenv('OPTION_A', u"Cat 🐺")
-option_b = os.getenv('OPTION_B', u"Dog 🐶")
+option_a = os.getenv('OPTION_A', "Cat 🐺")
+option_b = os.getenv('OPTION_B', "Dog 🐶")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
@@ -16,7 +22,13 @@ app = Flask(__name__)
 
 @app.route("/", methods=['POST','GET'])
 def hello():
-    rest_endpoint="http://" + os.environ["VOTING_API_SERVICE_HOST"] + ":" + os.environ["VOTING_API_SERVICE_PORT"]
+    """[summary]
+
+    Returns:
+        [type]: [description]
+    """
+    rest_endpoint="http://" + os.environ["VOTING_API_SERVICE_HOST"] + ":" +\
+        os.environ["VOTING_API_SERVICE_PORT"]
     voter_id = request.cookies.get('voter_id')
     if not voter_id:
         voter_id = hex(random.getrandbits(64))[2:-1]
@@ -39,18 +51,34 @@ def hello():
 
 @app.route("/result", methods=['GET'])
 def result():
+    """[summary]
+
+    Returns:
+        [type]: [description]
+    """
     return make_response(render_template('result/index.html'))
 
 
 @app.route("/votes", methods=['GET'])
 def votes():
-    rest_endpoint="http://" + os.environ["VOTING_API_SERVICE_HOST"] + ":" + os.environ["VOTING_API_SERVICE_PORT"]
+    """[summary]
+
+    Returns:
+        [type]: [description]
+    """
+    rest_endpoint="http://" + os.environ["VOTING_API_SERVICE_HOST"] + ":" +\
+        os.environ["VOTING_API_SERVICE_PORT"]
     response = requests.get(url=rest_endpoint + "/vote")
     print(response.content)
     return response.content
 
 @app.route('/templates/<path:path>')
 def send_js(path):
+    """[summary]
+
+    Returns:
+        [type]: [description]
+    """
     return send_from_directory('templates', path)
 
 
